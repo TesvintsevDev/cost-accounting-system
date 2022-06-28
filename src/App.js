aa63e61f62d002b42c981fbe61e00da76d5626b5
Eduard Tesvintsev <tesvintsev@gmail.com>
2022-06-28 23:23:07 +0300

    first components

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import './App.css';
+import CostItem from "./components/CostItem";
+
+function App() {
+
+    const costs = [
+        {
+            date: new Date(2022, 5, 16),
+            description: 'Холодос',
+            amount: '999.99'
+        },
+        {
+            date: new Date(2021, 9, 12),
+            description: 'MacBook Pro',
+            amount: '1799.50'
+        },
+        {
+            date: new Date(2022, 5, 15),
+            description: 'JBL Wave 200TWS',
+            amount: '49.99'
+        }
+    ]
+
+  return (
+    <div className="App">
+      <h1>MarketPlace</h1>
+        <CostItem date={costs[0].date} description={costs[0].description} amount={costs[0].amount}/>
+        <CostItem date={costs[1].date} description={costs[1].description} amount={costs[1].amount}/>
+        <CostItem date={costs[2].date} description={costs[2].description} amount={costs[2].amount}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/CostItem.jsx
@@ -0,0 +1,17 @@
+import "./CostItem.css"
+
+function CostItem(props) {
+    return (
+        <div className="cost-item">
+            <div>{props.date.toISOString()}</div>
+            <div className="cost-item__description">
+                <h2>
+                    {props.description}
+                </h2>
+                <div className="cost-item__price">${props.amount}</div>
+            </div>
+        </div>
+    );
+}
+
+export default CostItem;
