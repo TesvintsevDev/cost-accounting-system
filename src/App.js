4d3811485c8accb615f9406484ab926f4988b2c2
Edward <83923244+TesvintsevDev@users.noreply.github.com>
2022-06-30 19:41:29 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     const costs = [
         {
             date: new Date(2022, 5, 16),
-            description: 'Холодос',
+            description: 'Холодильник',
             amount: '999.99'
         },
         {
@@ -23,7 +23,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1>MarketPlace</h1>
+      <h1>Cost Accounting System</h1>
         <CostItem date={costs[0].date} description={costs[0].description} amount={costs[0].amount}/>
         <CostItem date={costs[1].date} description={costs[1].description} amount={costs[1].amount}/>
         <CostItem date={costs[2].date} description={costs[2].description} amount={costs[2].amount}/>
