7e8d55c523dabfcf9fe0b83d34d04b904fd8ef13
Eduard Tesvintsev <tesvintsev@gmail.com>
2022-07-03 22:51:39 +0300

    data transfer and component processing

--- src/App.js
+++ src/App.js
@@ -1,34 +1,30 @@
-import './App.css';
-import CostItem from "./components/CostItem";
+import Costs from "./components/Costs/Costs";
 
-function App() {
-
-    const costs = [
-        {
-            date: new Date(2022, 5, 16),
-            description: 'Холодильник',
-            amount: '999.99'
-        },
-        {
-            date: new Date(2021, 9, 12),
-            description: 'MacBook Pro',
-            amount: '1799.50'
-        },
-        {
-            date: new Date(2022, 5, 15),
-            description: 'JBL Wave 200TWS',
-            amount: '49.99'
-        }
-    ]
+const App = () => {
+  const costs = [
+    {
+      date: new Date(2020, 2, 12),
+      description: "Холодильник",
+      amount: 999.99,
+    },
+    {
+      date: new Date(2021, 11, 25),
+      description: "MacBook Pro",
+      amount: 3254.72,
+    },
+    {
+      date: new Date(2022, 5, 14),
+      description: "JBL Wave 200TWS",
+      amount: 49.99,
+    },
+  ];
 
   return (
-    <div className="App">
-      <h1>Cost Accounting System</h1>
-        <CostItem date={costs[0].date} description={costs[0].description} amount={costs[0].amount}/>
-        <CostItem date={costs[1].date} description={costs[1].description} amount={costs[1].amount}/>
-        <CostItem date={costs[2].date} description={costs[2].description} amount={costs[2].amount}/>
+    <div>
+      <h1>Система учета расходов</h1>
+      <Costs costs={costs} />
     </div>
   );
-}
+};
 
 export default App;
