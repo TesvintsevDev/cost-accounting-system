c4d99fdd4139b7f50f0ebbf43e765bfca1f9cc8b
Eduard Tesvintsev <tesvintsev@gmail.com>
2022-07-07 21:35:48 +0300

    add filter

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import Costs from "./components/Costs/Costs";
 import NewCost from "./components/NewCost/NewCost";
+import Costs from "./components/Costs/Costs";
 
 const App = () => {
   const costs = [
     {
+      id: "c1",
       date: new Date(2020, 2, 12),
       description: "Холодильник",
       amount: 999.99,
     },
     {
+      id: "c2",
       date: new Date(2021, 11, 25),
       description: "MacBook Pro",
       amount: 3254.72,
@@ -20,12 +22,19 @@ const App = () => {
     },
   ];
 
+  const addCostHandler = (cost) => {
+    console.log(cost);
+    console.log("App Component");
+  };
+
   return (
-    <div>
-      <NewCost />
-      <Costs costs={costs} />
-    </div>
+      <div>
+        <NewCost onAddCost={addCostHandler} />
+        <Costs costs={costs} />
+      </div>
   );
 };
 
 export default App;
+
+
